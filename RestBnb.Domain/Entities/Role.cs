cfcf7cec8f2f87868c39049d96d86fd0f58242e8
Wioletta Mańka <wioletta.manka728@gmail.com>
2020-04-12 16:45:43 +0200

    Add role model constructor

--- RestBnb.Domain/Entities/Role.cs
+++ RestBnb.Domain/Entities/Role.cs
@@ -4,6 +4,14 @@ namespace RestBnb.Core.Entities
 {
     public class Role
     {
+        public Role(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                Name = name;
+            }
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public IEnumerable<UserRole> UserRoles { get; set; }
