42a90b957f7247d7718daf62962864d697981258
Wioletta Mańka <wioletta.manka728@gmail.com>
2020-04-19 12:15:17 +0200

    Remove unecessary comment

--- RestBnb/Validators/Users/Commands/UpdateUserCommandValidator.cs
+++ RestBnb/Validators/Users/Commands/UpdateUserCommandValidator.cs
@@ -12,7 +12,6 @@ namespace RestBnb.API.Validators.Users.Commands
             CascadeMode = CascadeMode.StopOnFirstFailure;
 
             RuleFor(user => user.FirstName).NotEmpty();
-            //TODO: phone pattern not working
             RuleFor(user => user.PhoneNumber).Matches(RegexPatterns.User.PhoneNumber).WithMessage("It is not a valid phone number");
 
             RuleFor(user => user)
