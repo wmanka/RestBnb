27f8d8f54eba062ffffec551f068acd98a2612a6
Wioletta Mańka <wioletta.manka728@gmail.com>
2020-11-14 14:51:06 +0100

    Migrate client app to Angular v11

--- RestBnb/ClientApp/src/app/app-routing.module.ts
+++ RestBnb/ClientApp/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })],
     exports: [RouterModule],
     providers: []
 })
